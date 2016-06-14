26dbef15d9c994359dcb0b2313bf8bdda3cd9e68
Nelson Jeppesen <nelson@simplymeasured.com>
2016-06-14 14:52:28 -0700

    Apply settings in post install

--- Formula/my-macos.rb
+++ Formula/my-macos.rb
@@ -2,14 +2,16 @@ class MyMacos < Formula
   desc 'System-wide macOS UI tweak'
   url 'https://github.com/Jeppesen-io/homebrew-macos/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-macos'
-  version '1.1'
+  version '1.2'
 
   bottle :unneeded
 
   def install
     # Deploy
     bin.install 'bin/my-macos'
+  end
 
+  def post_install
     # Run
     `my-macos`
   end
