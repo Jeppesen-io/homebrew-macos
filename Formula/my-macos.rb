99d8a7ec3b49d287b51e9d99b7b0a810f27049d6
Nelson Jeppesen <nelson@simplymeasured.com>
2016-06-14 14:34:16 -0700

    Bump

--- Formula/my-macos.rb
+++ Formula/my-macos.rb
@@ -2,7 +2,7 @@ class MyMacos < Formula
   desc 'System-wide macOS UI tweak'
   url 'https://github.com/Jeppesen-io/homebrew-macos/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-macos'
-  version '0.3'
+  version '0.4'
 
   bottle :unneeded
 
