1ebf5f9e63a314beb65331b62bfd473d879570db
Nelson Jeppesen <nelson@simplymeasured.com>
2016-06-14 14:00:05 -0700

    Testing more and more!

--- Formula/my-macos.rb
+++ Formula/my-macos.rb
@@ -1,10 +1,11 @@
-class Mymacos < Formula
-  desc 'Various macOS UI tweaks'
-  bottle :unneeded
+class MyMacOS < Formula
+  desc "Weather in your terminal, with ANSI colors and Unicode symbols"
+  homepage "https://github.com/fcambus/ansiweather"
+  url "https://github.com/fcambus/ansiweather/archive/1.07.tar.gz"
+  sha256 "f9b377b23ecc9c2d3567424b300b8e370eb0959c9b1cd0828ba07ce38f2ef0a0"
+  head "https://github.com/fcambus/ansiweather.git"
 
-  stable do
-    version '0.91'
-  end
+  bottle :unneeded
 
   def install
 
@@ -105,9 +106,6 @@ class Mymacos < Formula
     # Restart finder
     `killall Finder`
 
-  end
-
-  test do
   end
 
 end
