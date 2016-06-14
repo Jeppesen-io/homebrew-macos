8b7064ade20cfd287f36f41ed6c2b2243d89619f
Nelson Jeppesen <nelson@simplymeasured.com>
2016-06-14 14:43:49 -0700

    First *working* version

--- Formula/my-macos.rb
+++ Formula/my-macos.rb
@@ -2,12 +2,18 @@ class MyMacos < Formula
   desc 'System-wide macOS UI tweak'
   url 'https://github.com/Jeppesen-io/homebrew-macos/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-macos'
-  version '0.4'
+  version '0.9'
 
   bottle :unneeded
 
+  # Deploy settings
   def install
     bin.install 'bin/my-macos'
+  end
+
+  # Run settings
+  test do
+    system 'my-macos'
   end
 
 end
